ede29db5d38070d251d5ede58976c6f0b4958cf5
“irwinj7” <Irwinjorge@pursuit.com>
2023-10-12 23:02:16 -0400

    some test

--- solution.js
+++ solution.js
@@ -1,5 +1,87 @@
 const { nums, words } = require("./data/data.js");
 
+class Node {
+  constructor(data){
+    this.data = data;
+    this.next = null
+  }
+}
+
+const firstNode = new Node(1);
+const secondNode = new Node(2);
+firstNode.next = secondNode;
+
+class LinkedList {
+  constructor(head = null){
+    this.head = head;
+  }
+  size() {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count++;
+      node = node.next;
+    }
+    return count
+  }
+  search(key){
+    let node = this.head;
+    while (node !== null && node.data !== key) {
+      node = node.next
+    }
+    return node
+  }
+  clear(){
+    this.head = null
+  }
+  getLast(){
+    let node = this.head;
+    if (!this.head) return null;
+    while (node.next){
+      node = node.next;
+    }
+    return node;
+  }
+  insert(data){
+    let newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode
+    } else {
+      newNode.next = this.head
+      this.head = newNode;
+    }
+  }
+  delete(data){
+    let node =  this.head;
+    let counter = 0;
+    while(node.data !== data && node.next){
+      counter++
+      node = node.next
+    }
+    let foundNode = node;
+    node = this.head;
+    for (let i = 1; i < counter; i++){
+      node = node.next;
+    }
+    node.next = foundNode.next;
+  }
+  getFirst(){
+    return this.head
+  }
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
 module.exports = {
   Node,
   LinkedList,
